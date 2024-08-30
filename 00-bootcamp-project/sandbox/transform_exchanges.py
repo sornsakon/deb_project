dfba6baa49ec664c3e169b02647ba9584b14b5ed
Kazz <75887967+sornsakon@users.noreply.github.com>
2024-08-30 09:55:02 +0000

    bro

---
+++ 00-bootcamp-project/sandbox/transform_exchanges.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+
+# Load data into a dataframe
+df = pd.read_csv("exchanges.csv")
+
+# Make columns snake case and lower case
+def change_case(str):
+    return "".join(["_" + i.lower() if i.isupper() else i for i in str]).lstrip("_")
+
+
+df.columns = df.columns.map(change_case)
+print(df.head())
+
+# Convert Unix timestamp to datetime in Asia/Bangkok
+df["updated_gmt_7"] = df["updated"].astype({"updated": "datetime64[ms]"}) \
+                        .dt.tz_localize("UTC") \
+                        .dt.tz_convert("Asia/Bangkok")
+print(df.head())
